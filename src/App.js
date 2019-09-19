a742ff73e71397fb7fc64f697da2b48f8590e02c
thecloudslyer <taylor.c.graham@gmail.com>
2019-09-19 10:11:02 -0400

    Basic Read one user.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,55 @@ import "./App.css";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { apiResponse: "" ,
+                        name:"",
+                        email:""};
     }
 
-    callAPI() {
+
+    userLookup() {
+        fetch("http://localhost:3001/users/4")
+            .then(res => res.json())
+            .then(json => {
+                var name1 = json[0].name;
+                var email1 = json[0].email
+
+                console.log("test json",json)
+                console.log("name: ",name1)
+                console.log("email: ", email1)
+                this.setState({name: name1})
+                this.setState({email: email1})
+
+                })
+
+
+
+
+
+
+
+
+    }
+
+
+
+    /*userLookup() {
         fetch("http://localhost:3001/users/1")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
 
-    }
+
+    }*/
+
+
 
     componentDidMount() {
-        this.callAPI();
+        console.log (this.userLookup());
+
+
+
+
     }
 
     render() {
@@ -27,7 +63,9 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <h1 className="App-intro">{this.state.apiResponse}</h1>
+                <h1 className="App-intro">{this.state.name}</h1>
+                    <h1 className="App-intro">{this.state.email}</h1>
+
             </div>
         );
     }
